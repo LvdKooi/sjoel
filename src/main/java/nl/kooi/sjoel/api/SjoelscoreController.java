f7a0934446b0d8e52c02d8a48ee5f3075fdef26a
Laurens van der Kooi <kooi.lv@tkppensioen.nl>
2021-01-16 21:40:52 +0100

    working!

--- src/main/java/nl/kooi/sjoel/api/SjoelscoreController.java
+++ src/main/java/nl/kooi/sjoel/api/SjoelscoreController.java
@@ -4,29 +4,30 @@ import lombok.RequiredArgsConstructor;
 import nl.kooi.sjoel.domain.Mapper;
 import nl.kooi.sjoel.domain.SjoelscoreService;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class SjoelscoreController {
     private final SjoelscoreService sjoelscoreService;
 
-    @RequestMapping("/sjoel")
-    public String initSubmitForm(Model model) {
+    @GetMapping("/sjoel")
+    public String sjoelscoreForm(Model model) {
         var sjoelscoreDto = new SjoelscoreDto();
-        model.addAttribute("sjoelscoreDto", sjoelscoreDto);
-        return "submitScoreForm";
+        model.addAttribute("sjoelscore", sjoelscoreDto);
+        return "sjoelscore";
     }
 
 
-    @PostMapping("/sjoel/score")
+    @PostMapping("/sjoel")
     @ResponseStatus(HttpStatus.OK)
-    public int submitScore(@Valid SjoelscoreDto score){
-
-        return sjoelscoreService.getPunten(Mapper.map(score));
+    public String submitScore(@ModelAttribute SjoelscoreDto score, Model model){
+        model.addAttribute("punten", String.valueOf(sjoelscoreService.getPunten(Mapper.map(score))));
+        return "result";
     }
 
 
