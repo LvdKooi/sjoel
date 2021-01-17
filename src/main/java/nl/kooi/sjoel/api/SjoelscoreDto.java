a00b040af7baf62a0b2d5b5117bca773cbdf174a
Laurens van der Kooi <kooi.lv@tkppensioen.nl>
2021-01-17 15:13:25 +0100

    checking not null

--- src/main/java/nl/kooi/sjoel/api/SjoelscoreDto.java
+++ src/main/java/nl/kooi/sjoel/api/SjoelscoreDto.java
@@ -4,22 +4,27 @@ import lombok.Data;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class SjoelscoreDto {
     @Min(value = 0, message = "Scores kleiner dan 0 zijn niet mogelijk bij sjoelen.")
     @Max(value = 32, message = "Scores groter dan 32 zijn niet mogelijk bij sjoelen.")
+    @NotNull(message = "Vul een score in.")
     private Integer scoresTwee;
 
     @Min(value = 0, message = "Scores kleiner dan 0 zijn niet mogelijk bij sjoelen.")
     @Max(value = 32, message = "Scores groter dan 32 zijn niet mogelijk bij sjoelen.")
+    @NotNull(message = "Vul een score in.")
     private Integer scoresDrie;
 
     @Min(value = 0, message = "Scores kleiner dan 0 zijn niet mogelijk bij sjoelen.")
     @Max(value = 32, message = "Scores groter dan 32 zijn niet mogelijk bij sjoelen.")
+    @NotNull(message = "Vul een score in.")
     private Integer scoresVier;
 
     @Min(value = 0, message = "Scores kleiner dan 0 zijn niet mogelijk bij sjoelen.")
     @Max(value = 32, message = "Scores groter dan 32 zijn niet mogelijk bij sjoelen.")
+    @NotNull(message = "Vul een score in.")
     private Integer scoresEen;
 }
